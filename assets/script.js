73656b34b2adb1cde08424af13c94998f8ccf32c
Charles Cox III <charlescoxc3@Charless-Mac-mini.local>
2022-10-05 11:17:46 -0700

    added HTML, CSS, Javascript, made timer and start button

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer-count")
+
+
+
+
+function startGame() {
+    timerCount = 75;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    startTimer()
+  }
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = "Time: " + timerCount;
+
+    }, 1000);
+  }
+
+  startButton.addEventListener("click", startGame);
